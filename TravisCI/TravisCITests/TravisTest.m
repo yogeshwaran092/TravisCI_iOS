3a2f6658b50502deb5d4c206b47107fd50358e4f
Yogeshwaran <yogeshwaran.m@identv.com>
2016-01-14 18:01:38 +0530

    test3

--- TravisCI/TravisCITests/TravisTest.m
+++ TravisCI/TravisCITests/TravisTest.m
@@ -19,21 +19,24 @@
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
+
+
+-(void)testStringForNil
+{
+    NSString *someString;
+    XCTAssertNil(someString,@"somestring was not nil");
+}
+
+-(void)testFalse
+{
+    int value1 = 1;
+    int value2 = 2 ;
+    XCTAssertFalse(value1 == value2,@"%d == %d should evaluate to false",value1,value2);
+}
+
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
 }
 
 @end
